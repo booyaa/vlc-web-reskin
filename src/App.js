35c433e055d4aa166a5bdebc09e8d56c26049f30
booyaa <booyaabooyaabooyaa@gmail.com>
2019-08-20 10:23:38 +0100

    (feature) view player status and title

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+const API = '/requests/'
+const DEFAULT_QUERY = 'status.json'
+// https://github.com/videolan/vlc/blob/master/share/lua/http/requests/README.txt
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playerState: [],
+      title: null,
+      isLoading: false,
+      error: null,
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+
+    fetch(API + DEFAULT_QUERY)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('something went wrong')
+        }
+      })
+      .then(data => this.setState({ playerState: data, title: data.information.category.meta.filename, isLoading: false }))
+      .catch(error => this.setState({ error, isLoading: false }));
+  }
+
+  render() {
+    const { playerState, title, isLoading, error } = this.state;
+
+    if (error) {
+      return <p>{error.message}</p>;
+    }
+
+    if (isLoading) {
+      return <p>Loading ...</p>;
+    }
+
+    return (
+
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            VLC is {playerState.state} - {title}
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
